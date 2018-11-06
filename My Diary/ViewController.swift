87d8a18730fc750ad498576c394eb941ec1d3438
kanyanee promma <medusawitch2@gmail.com>
2018-11-06 09:25:29 +0700

    Initial Commit

---
+++ My Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Diary
+//
+//  Created by Kanyanee P on 6/11/2561 BE.
+//  Copyright © 2561 Kanyanee P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
